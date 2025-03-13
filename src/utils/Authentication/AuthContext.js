857fba7010818c3ccaf81f19a49fdec3e77a7c47
Ryan <ryanthiago.dsantos@gmail.com>
2025-03-13 20:42:42 -0300

    tentando corrigir a requisição de auth

--- src/utils/Authentication/AuthContext.js
+++ src/utils/Authentication/AuthContext.js
@@ -30,7 +30,7 @@ export function AuthProvider({ children }) {
         async function checkAuth() {
             try {
                 const response = await fetch('https://star.api.ryandev.com.br/users/auth', {
-                    method: 'GET',
+                    method: 'POST',
                     credentials: 'include',
                 });
 
